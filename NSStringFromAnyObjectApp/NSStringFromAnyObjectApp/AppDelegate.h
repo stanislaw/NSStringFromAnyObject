f72bc526e0e92af38b2936dd336b024f3659819e
Stanislaw Pankevich <s.pankevich@gmail.com>
2014-04-26 16:27:03 +0400

    Re-add fresh working Xcode project

---
+++ NSStringFromAnyObjectApp/NSStringFromAnyObjectApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSStringFromAnyObjectApp
+//
+//  Created by Stanislaw Pankevich on 26/04/14.
+//  Copyright (c) 2014 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSStringFromAnyObjectApp/NSStringFromAnyObjectAppTests/NSStringFromAnyObjectAppTests.m
@@ -0,0 +1,34 @@
+//
+//  NSStringFromAnyObjectAppTests.m
+//  NSStringFromAnyObjectAppTests
+//
+//  Created by Stanislaw Pankevich on 26/04/14.
+//  Copyright (c) 2014 Stanislaw Pankevich. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NSStringFromAnyObjectAppTests : XCTestCase
+
+@end
+
+@implementation NSStringFromAnyObjectAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
